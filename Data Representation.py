2e94939b16b492c08ad4c723f52794d86fb2c8af
Josh1470 <jossol07@highgateschool.org.uk>
2021-09-20 12:07:02 +0100

    Added ability to change time series

--- Data Representation.py
+++ Data Representation.py
@@ -1,14 +1,22 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+#import numpy as np
 
 
-class stockData:
-    def __init__(self,stock):
-        self.stock = stock
-        ticker = yf.Ticker(self.stock)
-        ticker_history = ticker.history(period='max')
+class graphStockData:
+    def __init__(self):
+        self.time = 0
+        self.stockInfo = input('Enter Stock You Would Like to View')
+        graphStockData.change_time_series(self)
+
+    def change_time_series(self):
+        self.time = input('Enter time series you would like to view: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo?')
+        graphStockData.graph(self)
+
+    def graph(self):
+        ticker = yf.Ticker(self.stockInfo)
+        ticker_history = ticker.history(period=self.time)
         print((ticker_history['Open']))
 
         sf = ticker_history['Open']
@@ -23,6 +31,12 @@ class stockData:
         plt.show()
 
 
-y = stockData('AAPL')
+
+
+
+
+
+
+y = graphStockData()
 print(y)
 
