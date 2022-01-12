1bfff925320da2daeaec973a4ff67ea74b8ebf5a
jossol07 <Josh!470>
2022-01-12 10:21:13 +0000

    Added the ability to change frames

--- GUI_Stuff/FinanceGUI.py
+++ GUI_Stuff/FinanceGUI.py
@@ -8,23 +8,31 @@ class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        container = tk.Frame(self)
-        container.grid(row=0, column=0, sticky='news')
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        # container = tk.Frame(self)
+        # container.grid(row=0, column=0, sticky='news')
+        # container.grid_rowconfigure(0, weight=1)
+        # container.grid_columnconfigure(0, weight=1)
 
-        self.frames = {}
-        for F in (TitlePage, OneStock, TwoStock):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
-            frame.grid(row=0, column=0, sticky='news')
+        self.frames = {'Main Menu': TitlePage(self),
+                       'First Page': OneStock(self),
+                       'Second Page': TwoStock(self)}
+        # for F in (TitlePage, OneStock, TwoStock):
+        #     # page_name = F.__name__
+        #     frame = F(parent=container, controller=self)
+        #     self.frames[page_name] = frame
+        #     frame.grid(row=0, column=0, sticky='news')
 
-        self.show_frame("TitlePage")
+        self.show_frame('Main Menu')
 
     def show_frame(self, page_name):
         frame = self.frames[page_name]
-        frame.tkraise()
+        # frame.grid(row=0, column=0, sticky='news')
+        widgets = self.winfo_children()
+        for w in widgets:
+            if w.winfo_class() == 'Frame':
+                w.pack_forget()
+        frame.pack(expand=True, fill=tk.BOTH)
+        #frame.tkraise(self)
 
 
 
@@ -32,17 +40,17 @@ class App(tk.Tk):
 
 
 class TitlePage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+    def __init__(self, controller):
+        tk.Frame.__init__(self)
 
         self.controller = controller
-        self.title = tk.Label(text='Stock Grapher', font=('Arial', 14), bg='red')
+        self.title = tk.Label(self, text='Stock Grapher', font=('Arial', 14), bg='red')
 
 
-        self.oneStock = tk.Button(text='Click here to graph one stock', bg='Blue',
-                                  command=lambda: controller.show_frame("OneStock"))
+        self.oneStock = tk.Button(self, text='Click here to graph one stock', bg='Blue',
+                                  command=lambda: controller.show_frame("First Page"))
 
-        self.twoStocks = tk.Button(text='Click here to graph two stocks', bg='Pink',
+        self.twoStocks = tk.Button(self, text='Click here to graph two stocks', bg='Pink',
                                    command=lambda: controller.show_frame("TwoStock"))
 
         self.oneStock.grid(row=1, column=0, rowspan=5, sticky='s')
@@ -52,19 +60,19 @@ class TitlePage(tk.Frame):
 
 
 class OneStock(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+    def __init__(self, controller):
+        tk.Frame.__init__(self)
         self.controller = controller
         label = tk.Label(self, text="This is page 1")
         label.pack()
         button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("TitlePage"))
+                           command=lambda: controller.show_frame('Main Menu'))
         button.pack()
 
 
 class TwoStock(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+    def __init__(self, controller):
+        tk.Frame.__init__(self)
         self.controller = controller
         TwoStock = TSC
 
